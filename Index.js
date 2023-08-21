b1457926b577fdddefd89e85bc2fb214f4201a54
Shafaq Naz <107723802+shafaqimran1083@users.noreply.github.com>
2023-08-21 11:21:43 +0500

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Redirect to the cart page on GitHub Pages
+signupButton.addEventListener('click', function() {
+    window.location.href = 'cart.html';
+});
+
+// Redirect to the sign-in page on GitHub Pages
+loginButton.addEventListener('click', function() {
+    window.location.href = 'signin.html';
+});
