c4bf90f0047847a2b290657c70d00931c98fea0d
Shafaq Naz <107723802+shafaqimran1083@users.noreply.github.com>
2023-08-22 08:51:36 +0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -2,9 +2,9 @@ const signupButton = document.getElementById('getStartedButton');
 const loginButton = document.getElementById('loginButton'); 
 
 signupButton.addEventListener('click', function() {
-    window.location.href = 'cart.html';
+    window.location.href = 'Cart.html';
 });
 
 loginButton.addEventListener('click', function() {
-    window.location.href = 'signin.html';
+    window.location.href = 'Sign in.html';
 });
